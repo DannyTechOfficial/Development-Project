4b62ffe748eb8f07e673d32595cda365219c5ed9
Daniel Ball <me.danielball98@gmail.com>
2022-04-06 10:45:26 +0100

    Small Update 2
    Added small adjustments in prep for big stuff

--- Assets/Scripts/Frontline_Manager.cs
+++ Assets/Scripts/Frontline_Manager.cs
@@ -8,13 +8,11 @@ using System;
 public class Frontline_Manager : MonoBehaviour
 {
 
-    private static Frontline_Manager _instance;
-
     public GameObject Player;
     public GameObject playerScoreObject, GameOverObject;
     
     public float moveSpeed;
-    public int Health;
+    public int Health, maxScore;
     public bool isDead;
     public int Difficulty;
 
@@ -28,19 +26,9 @@ public class Frontline_Manager : MonoBehaviour
     private int intPlayerScore = 0;
 
     Ambulance_Controller PController;
-    public GameObject Collider1, Collider2, Collider3, Collider4, Collider5, Collider6;
-    public Collisions C1, C2, C3, C4, C5, C6; 
+    public GameObject Collider1, Collider2, Collider3, Collider4, Collider5, Collider6, Collider7, Collider8, Collider9, Collider10, Collider11, Collider12;
+    public Collisions C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12; 
         
-    
-    public static Frontline_Manager Instance
-    {
-        get
-        {
-            if (_instance is null)
-            Debug.LogError("game manager null");
-            return _instance;
-        }
-    }
 
     // Start is called before the first frame update
     void Start()
@@ -59,6 +47,13 @@ public class Frontline_Manager : MonoBehaviour
         C4 = Collider4.GetComponent<Collisions>();;
         C5 = Collider5.GetComponent<Collisions>();;
         C6 = Collider6.GetComponent<Collisions>();
+
+        C7 = Collider1.GetComponent<Collisions>();
+        C8 = Collider2.GetComponent<Collisions>();;
+        C9 = Collider3.GetComponent<Collisions>();;
+        C10 = Collider4.GetComponent<Collisions>();;
+        C11 = Collider5.GetComponent<Collisions>();;
+        C12 = Collider6.GetComponent<Collisions>();
         
         
     }
@@ -70,9 +65,9 @@ public class Frontline_Manager : MonoBehaviour
         PController.health = Health;
         PController.dead = isDead;
 
-        if (Convert.ToInt32(playerScore.text) == 100)
+        if (Convert.ToInt32(playerScore.text) == maxScore)
         {
-            Debug.Log("You have completed the demo ! !");
+            GameOverText.text = "Your reached the Max Score !   -   Well Done ! !";
         }
     }
 
@@ -84,20 +79,17 @@ public class Frontline_Manager : MonoBehaviour
             GameOverText.text = "You Crashed The Ambulance ! - Press ESC to go to menu.";
         }
     }
-
-    private void Awake() {
-        _instance = this;
-    }
-
     public void reachedMission(Collision2D other)
     {
         Debug.Log("Object Collectable and is Checkpoint");
-        other.gameObject.SetActive(false);
+        other.gameObject.transform.Translate(0.0f, 50.0f, 0.0f);
         intPlayerScore += other.gameObject.GetComponent<Collisions>().points;
         playerScore.text = Convert.ToString(intPlayerScore);
         Debug.Log(playerScore);
         missionCount += 1;
+        moveSpeed += 1.5f;
         loadMission();
+        if(missionCount == 6) missionCount = 0;
     }
 
     public void loadMission()
@@ -106,13 +98,22 @@ public class Frontline_Manager : MonoBehaviour
         switch (missionCount)
         {
             case 1:
-                Collider1.transform.Translate(15, 0, 0);
+                Collider1.transform.position = new Vector3(-15.61f, 11.92f, -1.0f);
                 break;
             case 2:
-                Collider2.transform.Translate(-16, 0, 0);
+                Collider2.transform.position = new Vector3(15.89f, -7.45f, -1.0f);
                 break;
             case 3:
-                Debug.Log("3");
+                Collider3.transform.position = new Vector3(15.89f, -7.45f, -1.0f);
+                break;
+            case 4:
+                Collider4.transform.position = new Vector3(0f, 0f, -1.0f);
+                break;
+            case 5:
+                Collider5.transform.position = new Vector3(15.89f, -7.45f, -1.0f);
+                break;
+            case 6:
+                Collider6.transform.position = new Vector3(0f, 0f, -1.0f);
                 break;
         }
     }
